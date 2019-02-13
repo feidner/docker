d8a0514096a9b27286f732b8be87750b7b8c7b2c
Henrik <henrik.feidner@siemens.com>
2019-02-13 23:52:07 +0100

    trennung tomcat - spring

---
+++ spring/src/main/java/hfe/Application.java
@@ -0,0 +1,10 @@
+package hfe;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackageClasses = {Application.class})
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("hello geral, pippa und mats - juhu");
+    }
+}
