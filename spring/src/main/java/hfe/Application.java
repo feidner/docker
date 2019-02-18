de1b3ad1d75e56922a126d5e0f25a146891f9bc2
Henrik <henrik.feidner@siemens.com>
2019-02-18 10:16:33 +0100

    jocker me up

--- spring/src/main/java/hfe/Application.java
+++ spring/src/main/java/hfe/Application.java
@@ -5,6 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackageClasses = {Application.class})
 public class Application {
     public static void main(String[] args) {
-        System.out.println("hello geral, pippa und mats - juhu");
+        System.out.println("hello geral, pippa und mats - juhu - jetzt fang i a");
     }
 }
